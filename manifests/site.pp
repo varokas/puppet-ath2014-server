4e29571829e7dff2817b94b3e7239ef9fa0e5190
root <root@agile66.(none)>
2014-04-14 20:09:23 -0400

    add ath2014 user

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,3 +10,17 @@ mongodb::db { 'ath2014':
   user => 'ath2014',
   password => hiera('mongo_password') 
 }
+
+$users_default = { 
+    ensure => "present",
+    managehome => true,
+    gid => "sudo",
+    shell => "/bin/bash",
+}
+$ath2014_user_password = hiera("ath2014_user_password") 
+$users = {
+  "ath2014" => {
+    password => generate('/bin/sh', '-c', "openssl passwd -1 ${ath2014_user_password} | tr -d '\n'") 
+  }
+}
+create_resources(user, $users, $users_default) 
